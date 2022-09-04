3d80c270a8ec3a246d94c1f479371d0a970453e5
khatuntsevke <kirill.khatuntsev@yandex.ru>
2022-09-04 13:52:54 +0500

    init vs project add cs-files

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+namespace MyApp
+{
+
+    public static class MyArrayListExtension
+    {
+        public static void Append(this ArrayList arrayList, object value)
+        {
+            arrayList.Add(value);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new PerformanceTester< List<int> >();
+
+            var l = new List<int>();
+            var al = new ArrayList();
+            var ll = new LinkedList<int>();
+
+            ll.Append(0);
+            l.Append(0);
+            al.Append(0);
+
+            
+
+        }
+    }
+}
+
